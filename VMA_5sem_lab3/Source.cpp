566d21070df2af132aa4996d567b028915774e6c
maxmilan <max231194@gmail.com>
2014-11-11 20:54:43 +0300

    Added sweep method

--- VMA_5sem_lab3/Source.cpp
+++ VMA_5sem_lab3/Source.cpp
@@ -34,6 +34,26 @@ double* fill_f(double* x, int x_number)
 	return f;
 }
 
+double* countSimpleSweep(int number, double* a, double* b, double* c, double* F)
+{
+	double* result = new double[number];
+	double* alpha = new double[number];
+	double* beta = new double[number];
+	alpha[0] = b[0]/c[0];
+	beta[0] = F[0]/c[0];
+	double del;
+	for(int k=0;k<number-1;++k)
+	{
+		del = c[k+1] - alpha[k]*a[k];
+		alpha[k+1] = b[k+1]/del;
+		beta[k+1] = (F[k+1] + beta[k]*a[k])/del;
+	}
+	result[number - 1] = beta[number-1];
+	for(int i=number-2;i>=0;--i)
+		result[i] = alpha[i]*result[i+1] + beta[i];
+	return result;
+}
+
 int main()
 {
 	int n = 20;
